58ce4d5434a49265618cb140cc6bcf9a63a3839c
ijustwebdev <thestachehouse@mail.com>
2022-10-15 02:42:07 -0400

    localstorage determines if an api call is made

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import Footer from "./Footer.js"
 export default function App(){
 
     // create our state and setstate that holds the current kanji
-    const [currentKanji, setCurrentKanji] = useState({})
+    const [currentKanji, setCurrentKanji] = useState(
+        JSON.parse(localStorage.getItem("kanjiobj")) || {}
+    )
     // fetch a kanji, will be randomized later unless I can think of 
     // an easy search method. maybe random but by grade or other category
-    useEffect(() => {
-    const fetchData = async () => {
-        const response = await fetch('https://kanjiapi.dev/v1/kanji/裁')
-        const newData = await response.json()
-        setCurrentKanji(newData)
+    useEffect(() => { 
+        if(Object.keys(currentKanji).length === 0){
+        const fetchData = async () => {
+            const response = await fetch('https://kanjiapi.dev/v1/kanji/裁')
+            const newData = await response.json()
+            localStorage.setItem("kanjiobj", JSON.stringify(newData))
+            setCurrentKanji(newData)
+            }
+            fetchData()
+            console.warn("currentKanji Set")
         }
-        fetchData()
-    },[])
+        else{
+            console.warn("currentKanji Full")
+            console.log(currentKanji)
+        }
+    },[currentKanji])
 
     return(
         <div id="flexWrapper">
